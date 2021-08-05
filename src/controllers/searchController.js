c8dddc93e29ea472ea78230d110ce165722a1856
multicalor <d.tretiuk@gmail.com>
2021-08-05 14:34:10 +0300

    elasticsearch add first index

--- src/controllers/searchController.js
+++ src/controllers/searchController.js
@@ -1,25 +1,45 @@
-const searchService = require('../services/searchService')
+const elasticsearchService = require('../services/elasticSearchService')
 const axios = require('axios')
+
 const { Client } = require('@elastic/elasticsearch')
 const client = new Client({ node: 'http://localhost:9200' })
 
 
-const APIKEY = 'dHOPJtw6qvMMMvE1EXy7EALVEORxtgoh'
+const API_KEY = 'dHOPJtw6qvMMMvE1EXy7EALVEORxtgoh'
+// let BASE_URL = 
 
 class SearchController {
 
   async search(req, res) {
-    const { size, searchText } = req.body;
+    console.log(req.params)
+    const { size, searchText } = req.params;
+
     console.log(size, searchText)
-    const url = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchText}&api-key=${APIKEY}`
-    const test = axios.get(url)
-      // .then(response => (response) => {
-      //   return res.json(response.data.response.docs[1]);
-      // });
-      //console.log(response.data.response.docs[1])
-    console.log(test.then((req => {res.json(req.data.response.docs[1].multimedia.slice(+size))})))//.slice(size)
-    // const { id } = req.params;
-    // const {coast, products} = await basketService.create(req.body, id);
+    const url = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchText}&api-key=${API_KEY}`
+    const rawData = await axios.get(url);
+    const data = rawData.data.response.docs;
+    
+    // const result = await elasticsearchService.run(data, 'putin', size=1).catch(console.log)
+    async function run (data, searchText, size) {
+      await client.index({
+        index: 'data',
+        id: '1',
+        body: {
+          data,
+        }
+      })
+      const { body } = await client.exists({
+        index: 'data',
+        id: 1
+      })
+  
+      console.log(body)
+    }
+
+    run(data, searchText, size).catch(console.log)
+    
+
+    res.json('result')
     
   }
 
