576416c9d1c84c3db0b6f2cda4280ca71eeb4473
multicalor <d.tretiuk@gmail.com>
2021-08-06 16:24:01 +0300

    fix api book link

--- src/controllers/searchController.js
+++ src/controllers/searchController.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const axios = require('axios')
 const { arrToBulk } = require('../utils/arrToBulk')
 const { Client } = require('@elastic/elasticsearch')
@@ -5,8 +6,8 @@ const client = new Client({ node: 'http://localhost:9200' })
 
 
 
-const API_KEY = 'dHOPJtw6qvMMMvE1EXy7EALVEORxtgoh'
-// let BASE_URL = 
+const API_KEY = process.env.API_KEY //'dHOPJtw6qvMMMvE1EXy7EALVEORxtgoh'
+
 
 class SearchController {
 
@@ -18,66 +19,65 @@ class SearchController {
         id: elastic_id
       })
       console.log(body)
-  
-      res.send({status:200, data: { elasic_id:body._id, body: body._source }})
+
+      res.send({ status: 200, data: { elasic_id: body._id, body: body._source } })
     } catch (e) {
-      return res.status(500).json({status: Error, ditaile: e.message});
+      return res.status(500).json({ status: Error, ditaile: e.message });
     }
 
   }
 
   async apiSearch(req, res) {
-    try{
-    const { size, searchText } = req.params;
-    const url = `https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?title=${searchText}&api-key=${API_KEY}`
-    let books;
-    
+    try {
+      const { size, searchText } = req.params;
+      const url = `https://api.nytimes.com/svc/books/v3/lists/best-sellers/history.json?title=${searchText}&api-key=${API_KEY}`
+
       const rawData = await axios.get(url);
-      books = rawData.data.results.books.splice(0, +size)
-      // if(books.length === 0) return res.status(500).json({status: Error, ditaile: "no books on your request"});
 
+      const books = rawData.data.results.splice(0, +size)
 
-
-
-    const result = await arrToBulk(books, 'books', client)
-    console.log(result)
-    res.status(200).json({status: 200, data: result})
-  }catch(e){
-    return res.status(500).json({status: Error, ditaile: "Api book error"});
-  }
+      if (books.length === 0) return res.status(500).json({ status: Error, ditaile: "no books on your request" });
+      console.log(books)
+      const result = await arrToBulk(books, 'books', client)
+      console.log('books------->')
+      console.log(result)
+      res.status(200).json({ status: 200, data: result })
+    } catch (e) {
+      return res.status(500).json({ status: Error, ditaile: e.message });//"Api book error"
+    }
   }
 
   async elasticSearch(req, res) {
     try {
-    const {size, searchText } = req.params;
-  
-       const { body: response } = await client.search({
+      const { size, searchText } = req.params;
+
+      const { body: response } = await client.search({
         index: "books",
         body: {
           size,
           query: {
             multi_match: {
               query: searchText,
-              
+
               fields: ['title', 'description']
             }
           }
         }
-  
-      })
 
-      if(response.hits.hits.length === 0) return res.status(500).json({status: Error, ditaile: "no books on your request"});
+      })
+      console.log(response.hits)
+      if (response.hits.hits.length === 0) return res.status(500).json({ status: Error, ditaile: "no books on your request" });
 
       const result = response.hits.hits.map((element) => {
         let body = {}
         body = element._source
-  
-        return {elasic_id:element._id, body}
+
+        return { elasic_id: element._id, body }
       });
-  
-      return res.status(200).json({status: 200, data: result});
+
+      return res.status(200).json({ status: 200, data: result });
     } catch (e) {
-      return res.status(500).json({status: Error, ditaile: "data base error"});
+      return res.status(500).json({ status: Error, ditaile: "data base error" });
     }
 
 
@@ -92,13 +92,13 @@ class SearchController {
         index: "books",
         id: elastic_id,
         body: {
-          doc:body
+          doc: body
         }
       })
       console.log(response)
       return res.status(200).json(response);
     } catch (e) {
-      res.status(500).send({status:Error, message: e.message})
+      res.status(500).send({ status: Error, message: e.message })
     }
 
   }
@@ -108,16 +108,16 @@ class SearchController {
     try {
       const { elastic_id } = req.body;
       console.log(elastic_id)
-      const {body} = await client.delete({
+      const { body } = await client.delete({
         index: "books",
         id: elastic_id
       })
 
       console.log(body)
-      res.status(200).send({status:200, message: "delete"})
+      res.status(200).send({ status: 200, message: "delete" })
     } catch (e) {
       console.error(e.message)
-      res.status(500).send({status:Error, message: "no record according to your request"})
+      res.status(500).send({ status: Error, message: "no record according to your request" })
     }
 
   }
