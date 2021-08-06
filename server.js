064ff1f52cc84d0273f0f77312515a4e63c6d80f
multicalor <d.tretiuk@gmail.com>
2021-08-06 21:57:58 +0300

    complete simple error test

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
 const router = require('./src/routes/index');
-
 const PORT = process.env.PORT || 5000;
+
+//error handle
+const ApiError = require('./src/error/ApiError')
+const errorHandler = require('./src/middleware/ErrorHandlingMiddleware')
 
 const app = express();
 
@@ -15,8 +18,18 @@ app.use('/api', router);
 
 
 app.get('/', (req, res) => {
-  res.status(200).json({message: 'WORKING!!!'})
+  try {
+    throw  ApiError.forbidden('test error')
+
+    res.status(200).json({message: 'WORKING!!!'})
+  } catch (e) {
+    return res.status(e.status).json({message: e.message})
+  }
+  
+  
 })
+
+app.use(errorHandler);
 
 const start = async () => {
   try{
