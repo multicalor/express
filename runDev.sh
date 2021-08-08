60600a4c1141341cefbcdd712691714474524d4f
multicalor <d.tretiuk@gmail.com>
2021-08-08 23:37:57 +0300

    upgrade runDev.sh

--- runDev.sh
+++ runDev.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 curl -fsSL https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
 echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee -a /etc/apt/
 sudo apt update
